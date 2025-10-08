df6adfa3cb68f983e229847c1d6517283b7519c3
Jayson <marvinjaysonaustria17@gmail.com>
2025-10-08 14:42:27 +0800

    Exercise 4: Implemented dbt Silver layer transformations

--- dbt/models/gold/dim_date.sql
+++ dbt/models/gold/dim_date.sql
@@ -2,16 +2,17 @@
 
 with dates as (
     select 
-        date_add(date '2020-01-01', interval n day) as date_day
-    from range(0, 365 * 5)
+        date '2020-01-01' + range * interval '1 day' as date_day
+    from range(0, 365 * 5)  -- 5 years of dates
 )
+
 select
     date_day,
     extract(year from date_day) as year,
     extract(month from date_day) as month,
     extract(day from date_day) as day,
-    to_char(date_day, 'Day') as day_name,
-    to_char(date_day, 'Month') as month_name,
-    week(date_day) as week_of_year,
+    strftime(date_day, '%A') as day_name,   -- full day name
+    strftime(date_day, '%B') as month_name, -- full month name
+    strftime(date_day, '%W')::int + 1 as week_of_year,  -- week number
     case when extract(dow from date_day) in (6,0) then true else false end as is_weekend
 from dates
